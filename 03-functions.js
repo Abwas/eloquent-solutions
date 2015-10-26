b8f81ad71c64d07b1f26b1f47bff206a116359ac
Eric Douglas <eric.douglas.mail@gmail.com>
2015-10-26 17:14:00 -0200

    Add exercise minimum - ch03

---
+++ 03-functions.js
@@ -0,0 +1,7 @@
+export default function(first, last) {
+  if (first > last) {
+    return last;
+  }
+
+  return first;
+};
---
+++ test/03-functions.spec.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+import minimum from '../03-functions';
+
+const expect = chai.expect;
+
+describe('#Chapter 3 - Functions', function() {
+  describe('#Exercise 1 - Minimum', function() {
+    it('Should takes two arguments and return their minimum', function() {
+      expect(minimum(0, 10)).to.equal(0);
+      expect(minimum(0, -10)).to.equal(-10);
+    });
+  });
+});
